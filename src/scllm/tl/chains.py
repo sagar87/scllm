41b4e8af369507ccdf2150b1961c1c4fa5de4393
Harald Vohringer <harald.voeh@gmail.com>
2025-03-05 13:36:31 +0100

    updated tutorials

--- src/scllm/tl/chains.py
+++ src/scllm/tl/chains.py
@@ -1,23 +1,7 @@
-from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
 from langchain_core.runnables.base import RunnableEach, RunnableLambda, RunnableParallel
-from langchain_core.runnables.branch import RunnableBranch
 
 from .parser import CellTypeParser
 from .prompts import CellAnnotationPrompt
-
-
-def SampleChain(chain: RunnableLambda, num_samples: int):
-    # runs a chain num_samples times
-    inits = [i for i in range(num_samples)]
-
-    return RunnableEach(
-        bound=RunnableParallel(
-            {
-                "init": RunnableLambda(lambda x: x),
-                "result": chain,
-            }
-        )
-    )
 
 
 def CellTypeAnnotationChain(llm):
@@ -41,23 +25,3 @@ def CellTypeAnnotationChain(llm):
             }
         )
     )
-
-
-# recevies a list of genes and returns a list of cell types
-# def CellTypeAnnotationChain(llm):
-#     return RunnableLambda(lambda x: CellAnnotationPrompt.format_prompt(
-#         genes=", ".join(x), format_instructions=CellTypeParser.get_format_instructions()
-#     )) | llm | CellTypeParser
-
-
-# cell_type_branch = (
-#     RunnableLambda(
-#         lambda x: _analyze_genelist(x, output_parser.get_format_instructions())
-#     )
-#     | llm
-#     | output_parser
-# )
-#     chain = RunnableLambda(lambda x: x * num_samples) | RunnableEach(
-#         bound=cell_type_branch
-#     )
-#     res = chain.invoke(list(cluster_genes.values()))
