6322b29efe12a78f1e59629c380c609394415629
Harald Vohringer <harald.voeh@gmail.com>
2025-03-06 17:14:40 +0100

    validation function for top_genes argument

--- src/scllm/tl/utils.py
+++ src/scllm/tl/utils.py
@@ -9,3 +9,12 @@ def _prepare_mapping(df: pd.DataFrame, identity: str, target: str):
         .to_dict()
     )
     return mapping
+
+
+def _validate_top_genes(top_genes: int, num_genes: int):
+    if top_genes <= 0:
+        raise ValueError("top_genes must be greater than 0")
+    if top_genes > num_genes:
+        raise ValueError("top_genes must be less than or equal to the number of genes")
+
+    return top_genes
