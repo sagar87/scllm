3711d4e63f9e398e0ac8167788a21b7750bd34e2
Harald Vohringer <harald.voeh@gmail.com>
2025-03-07 21:10:43 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2025, Harald Vohringer"
 author = "Harald Vohringer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.4"
+release = "0.1.5"
 
 
 # -- General configuration ---------------------------------------------------
