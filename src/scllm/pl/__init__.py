4d8d07ec6c78aca316dd3c919699d99500c6a64b
Harald Vohringer <harald.voeh@gmail.com>
2025-03-09 09:28:49 +0100

    add facto_rridge function

--- src/scllm/pl/__init__.py
+++ src/scllm/pl/__init__.py
@@ -1,4 +1,5 @@
 from .factor_embedding import factor_embedding
+from .factor_ridge import factor_ridge
 from .factor_stripplot import factor_stripplot
 
-__all__ = ["factor_embedding", "factor_stripplot"]
+__all__ = ["factor_embedding", "factor_stripplot", "factor_ridge"]
