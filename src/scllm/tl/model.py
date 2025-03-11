b30a987a2648be2e3897a5b0957ccc2e079a5c44
Harald Vohringer <harald.voeh@gmail.com>
2025-03-11 20:46:56 +0100

    contains now all models

--- src/scllm/tl/model.py
+++ src/scllm/tl/model.py
@@ -1,108 +1,13 @@
-from abc import ABC, abstractmethod
-from functools import partial
 from typing import Literal
 
-import scanpy as sc
 from langchain_core.output_parsers.string import StrOutputParser
 
-from .chains import _description_chain, _term_chain
-from .factor_annotation import _prepare_chain_data, _validate_factors, _validate_sign
+from .base import BaseModel, ClusterMixin, DescriptionMixin, FactorMixin, TermMixin
 from .parser import (
     _multiple_term_parser,
     _term_parser,
 )
 from .prompts import _term_prompt
-from .utils import _prepare_cluster_data
-
-
-class BaseModel(ABC):
-
-    @abstractmethod
-    def _get_prompt(self):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def _get_parser(self):
-        raise NotImplementedError()
-
-
-class FactorMixin:
-    def _prepare(self, adata: sc.AnnData):
-        data = _prepare_chain_data(
-            adata,
-            self.varm_key,
-            self.factors,
-            "+",
-            self.top_items,
-            self.num_samples,
-        )
-        if self.sign == "both":
-            data_neg = _prepare_chain_data(
-                adata,
-                self.varm_key,
-                self.factors,
-                "-",
-                self.top_items,
-                self.num_samples,
-            )
-            data += data_neg
-
-        # Ugly workaround for now
-        data = [{"data": d_i["genes"], **d_i} for d_i in data]
-        return data
-
-    def fit(self, adata, llm):
-        num_factors = adata.varm[self.varm_key].shape[1]
-        # num_features = adata.shape[1]
-        self.sign = _validate_sign(self.sign)
-        self.factors = _validate_factors(self.factors, num_factors)
-
-        data = self._prepare(adata)
-        chain = self._get_chain()
-        res = chain(llm).invoke(data)
-        return res
-
-
-class ClusterMixin:
-    """
-    Preprares cluster data.
-    """
-
-    def _prepare(self, adata: sc.AnnData):
-        data = _prepare_cluster_data(
-            adata,
-            self.cluster_key,
-            top_items=self.top_items,
-            num_samples=self.num_samples,
-        )
-        return data
-
-    def fit(self, adata, llm):
-        data = self._prepare(adata)
-        chain = self._get_chain()
-        res = chain(llm).invoke(data)
-        return res
-
-
-class TermMixin:
-    """
-    Perpares chains for term-like queries.
-    """
-
-    def _get_chain(self):
-        prompt = self._get_prompt()
-        parser = self._get_parser()
-
-        return partial(_term_chain, prompt=prompt, parser=parser)
-
-
-class DescriptionMixin:
-
-    def _get_chain(self):
-        prompt = self._get_prompt()
-        parser = self._get_parser()
-
-        return partial(_description_chain, prompt=prompt, parser=parser)
 
 
 class ClusterAnnotation(BaseModel, ClusterMixin, TermMixin):
@@ -137,7 +42,7 @@ class ClusterAnnotation(BaseModel, ClusterMixin, TermMixin):
         return _term_parser(self.term, self.feature)
 
 
-class ClusterAnnotationTerms(BaseModel, ClusterMixin, TermMixin):
+class ClusterTerms(BaseModel, ClusterMixin, TermMixin):
     def __init__(
         self,
         cluster_key: str,
@@ -170,7 +75,7 @@ class ClusterAnnotationTerms(BaseModel, ClusterMixin, TermMixin):
         return _multiple_term_parser(self.term, self.feature, self.num_terms)
 
 
-class ClusterAnnotationDescription(BaseModel, ClusterMixin, DescriptionMixin):
+class ClusterDescription(BaseModel, ClusterMixin, DescriptionMixin):
     def __init__(
         self,
         cluster_key: str,
@@ -241,7 +146,7 @@ class FactorAnnotation(BaseModel, TermMixin, FactorMixin):
         return _term_parser(self.term, self.feature)
 
 
-class FactorAnnotationTerms(BaseModel, TermMixin, FactorMixin):
+class FactorTerms(BaseModel, TermMixin, FactorMixin):
     def __init__(
         self,
         varm_key: str,
