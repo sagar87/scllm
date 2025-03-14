17788a018e613398d8380825912ae6ea974eee97
Harald Vohringer <harald.voeh@gmail.com>
2025-03-14 05:11:32 +0100

    remove fields which are know in BaseClass, update imports

--- src/scllm/tl/model.py
+++ src/scllm/tl/model.py
@@ -1,33 +1,43 @@
 from typing import Literal
 
+import numpy as np
 import pandas as pd
 from langchain_core.output_parsers.string import StrOutputParser
 
-from .base import BaseModel, ClusterMixin, DescriptionMixin, FactorMixin, TermMixin
+from .base import (
+    BaseModel,
+    ClusterMixin,
+    DescriptionMixin,
+    FactorMixin,
+    TermMixin,
+    TermsMixin,
+)
 from .parser import (
     _multiple_term_parser,
     _term_parser,
 )
 from .prompts import _term_prompt
-from .utils import _prepare_mapping
+from .utils import _prepare_mapping, _prepare_var_names
 
 MAPPING = "mapping"
 
 
-class ClusterAnnotation(BaseModel, ClusterMixin, TermMixin):
+class ClusterAnnotation(ClusterMixin, TermMixin, BaseModel):
     def __init__(
         self,
         cluster_key: str,
         top_items: int = 30,
         num_samples: int = 1,
         term: str = "cell type",
-        feature: str = "gene",
+        feature: str = "genes",
         preface: str = "You are an expert computational biologist who analysis single-cell RNA-seq data",
         epilogue: str | None = None,
         key_added: str = "cluster_annotation",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.cluster_key = cluster_key
         self.top_items = top_items
         self.num_samples = num_samples
@@ -35,12 +45,6 @@ class ClusterAnnotation(BaseModel, ClusterMixin, TermMixin):
         # prompot related
         self.term = term
         self.feature = feature
-        self.preface = preface
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = (
@@ -53,8 +57,16 @@ class ClusterAnnotation(BaseModel, ClusterMixin, TermMixin):
     def _get_parser(self):
         return _term_parser(self.term, self.feature)
 
+    def _postprocess(self, adata):
+        df = pd.DataFrame(self.results_)
+        mapping = _prepare_mapping(df, "group", "term")
+        var_names = _prepare_var_names(df, mapping)
 
-class ClusterTerms(BaseModel, ClusterMixin, TermMixin):
+        adata.obs[self.key_added] = adata.obs[self.cluster_key].astype(str).map(mapping)
+        adata.uns[self.key_added] = {MAPPING: mapping, "var_names": var_names}
+
+
+class ClusterTerms(ClusterMixin, TermsMixin, BaseModel):
     def __init__(
         self,
         cluster_key: str,
@@ -68,21 +80,15 @@ class ClusterTerms(BaseModel, ClusterMixin, TermMixin):
         key_added: str = "cluster_terms",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.cluster_key = cluster_key
         self.term = term
         self.feature = feature
         self.top_items = top_items
         self.num_samples = num_samples
         self.num_terms = num_terms
-
-        # prompt related
-        self.preface = preface
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = f"Given the following {self.feature} identify the {self.num_terms} most likely {self.term}."
@@ -94,8 +100,13 @@ class ClusterTerms(BaseModel, ClusterMixin, TermMixin):
     def _get_parser(self):
         return _multiple_term_parser(self.term, self.feature, self.num_terms)
 
+    def _postprocess(self, adata):
+        mapping = {item["group"]: item["term"] for item in self.results_}
+        adata.uns[self.key_added] = {MAPPING: mapping}
+        # pass
 
-class ClusterDescription(BaseModel, ClusterMixin, DescriptionMixin):
+
+class ClusterDescription(ClusterMixin, DescriptionMixin, BaseModel):
     def __init__(
         self,
         cluster_key: str,
@@ -108,19 +119,15 @@ class ClusterDescription(BaseModel, ClusterMixin, DescriptionMixin):
         key_added: str = "cluster_description",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.cluster_key = cluster_key
         self.top_items = top_items
         self.num_samples = 1
 
         # prompt related
-        self.preface = preface
         self.feature = feature
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = f"You are given the following {self.feature}."
@@ -135,8 +142,12 @@ class ClusterDescription(BaseModel, ClusterMixin, DescriptionMixin):
     def _get_parser(self):
         return StrOutputParser()
 
+    def _postprocess(self, adata):
+        mapping = {item["group"]: np.str_(item["target"]) for item in self.results_}
+        adata.uns[self.key_added] = {MAPPING: mapping}
 
-class FactorAnnotation(BaseModel, TermMixin, FactorMixin):
+
+class FactorAnnotation(TermMixin, FactorMixin, BaseModel):
     def __init__(
         self,
         varm_key: str,
@@ -151,7 +162,9 @@ class FactorAnnotation(BaseModel, TermMixin, FactorMixin):
         key_added: str = "factor_annotation",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.varm_key = varm_key
         self.factors = factors
         self.sign = sign
@@ -161,12 +174,6 @@ class FactorAnnotation(BaseModel, TermMixin, FactorMixin):
         # prompt params
         self.term = term
         self.feature = feature
-        self.preface = preface
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = (
@@ -179,15 +186,16 @@ class FactorAnnotation(BaseModel, TermMixin, FactorMixin):
     def _get_parser(self):
         return _term_parser(self.term, self.feature)
 
-    def _postprocess(self):
+    def _postprocess(self, adata):
         df = pd.DataFrame(self.results_).assign(
             id=lambda df: df.apply(lambda row: row.factor + row.sign, 1)
         )
         mapping = _prepare_mapping(df, "id", "term")
-        return {MAPPING: mapping}
+
+        adata.uns[self.key_added] = {MAPPING: mapping}
 
 
-class FactorTerms(BaseModel, TermMixin, FactorMixin):
+class FactorTerms(TermsMixin, FactorMixin, BaseModel):
     def __init__(
         self,
         varm_key: str,
@@ -203,7 +211,9 @@ class FactorTerms(BaseModel, TermMixin, FactorMixin):
         key_added: str = "factor_terms",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.varm_key = varm_key
         self.factors = factors
         self.sign = sign
@@ -214,12 +224,6 @@ class FactorTerms(BaseModel, TermMixin, FactorMixin):
         # prompt params
         self.term = term
         self.feature = feature
-        self.preface = preface
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = f"Given the following {self.feature} identify the {self.num_terms} most likely {self.term}."
@@ -232,7 +236,7 @@ class FactorTerms(BaseModel, TermMixin, FactorMixin):
         return _multiple_term_parser(self.term, self.feature, self.num_terms)
 
 
-class FactorDescription(BaseModel, FactorMixin, DescriptionMixin):
+class FactorDescription(FactorMixin, DescriptionMixin, BaseModel):
     def __init__(
         self,
         varm_key: str,
@@ -247,20 +251,15 @@ class FactorDescription(BaseModel, FactorMixin, DescriptionMixin):
         key_added: str = "factor_description",
         copy: bool = False,
     ):
-        super().__init__()
+        super().__init__(
+            preface=preface, epilogue=epilogue, key_added=key_added, copy=copy
+        )
         self.varm_key = varm_key
         self.factors = factors
         self.sign = sign
         self.top_items = top_items
         self.feature = feature
         self.num_samples = 1
-
-        self.preface = preface
-        self.epilogue = epilogue
-
-        # adata related
-        self.key_added = key_added
-        self.copy = copy
 
     def _get_prompt(self):
         prologue = f"You are given the following {self.feature}."
